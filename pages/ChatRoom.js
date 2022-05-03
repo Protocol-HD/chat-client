680a0288f74ac33afbc96cf63df266266318c19e
Arthur <gudehdu10@gmail.com>
2022-05-03 19:35:44 +0900

    test room create leave

--- pages/ChatRoom.js
+++ pages/ChatRoom.js
@@ -10,21 +10,36 @@ function ChatRoom() {
         cors: { origin: '*' }
     });
 
+    const sendMessage = () => {
+        socket.emit('ClientToServer', {
+            room: document.getElementById('join-test-input').value,
+            message: document.getElementById('send-test-input').value
+        });
+        document.getElementById('send-test-input').value = '';
+    }
+
+    const joinRoom = () => {
+        socket.emit('createRoom', document.getElementById('join-test-input').value);
+    }
+
+    const leaveRoom = () => {
+        socket.emit('leaveRoom', document.getElementById('join-test-input').value);
+    }
+
     useEffect(() => {
         socket.on('ServerToClient', data => {
             setResponse(data);
         })
     }, [])
 
-    const sendMessage = () => {
-        socket.emit('ClientToServer', document.getElementById('send-test-input').value);
-        document.getElementById('send-test-input').value = '';
-    }
-
 
     return (
         <>
+            <input id="join-test-input"></input>
+            <button onClick={() => joinRoom()}>join</button>
+            <button onClick={() => leaveRoom()}>leave</button>
             <div>{response}</div>
+            <input id="room-test-input"></input>
             <input id="send-test-input"></input>
             <button onClick={() => sendMessage()}>send</button>
         </>
